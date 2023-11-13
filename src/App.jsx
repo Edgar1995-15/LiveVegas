e50a873e29b7e9051db5a0740f669df6f7e90e1d
Edgar <edgar.khojayan@livevegas.live>
2023-11-13 12:39:19 +0400

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import Footer from "./components/footer/Footer";
-import Header from "./components/header/Header";
 import About from "./pages/about/About";
 import ContactUs from "./pages/contacts/ContactUs";
 import Games from "./pages/games/Games";
